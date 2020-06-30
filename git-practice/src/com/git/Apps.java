719af2763fae63ae8c8dee2c35f5504476069b86
bijay1510 <67598928+bijay1510@users.noreply.github.com>
2020-06-30 17:06:53 +0530

    Update Apps.java

--- git-practice/src/com/git/Apps.java
+++ git-practice/src/com/git/Apps.java
@@ -3,8 +3,7 @@ package com.git;
 public class Apps {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Hey there!");
+		System.out.println("Hello from dev branch!");
 	}
 
 }
