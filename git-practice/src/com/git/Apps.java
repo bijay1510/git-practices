49a6d6b73f9e5089ab605efd0a3ca7441733de81
bijay1510 <67598928+bijay1510@users.noreply.github.com>
2020-07-21 20:04:35 +0530

    Update Apps.java
    updated code

--- git-practice/src/com/git/Apps.java
+++ git-practice/src/com/git/Apps.java
@@ -6,8 +6,7 @@ import java.util.List;
 public class Apps {
 
 	public static void main(String[] args) {
-		List<Customer> customers = Arrays.asList(new Customer("C1","AAA"), new Customer("C2","BBB"), new Customer("C3","CCC"));
-		customers.forEach(System.out::println);
+		Arrays.asList(new Customer("C1","AAA"), new Customer("C2","BBB"), new Customer("C3","CCC")).forEach(System.out::println);
 	}
 
 }
